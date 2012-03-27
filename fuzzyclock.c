fe842a77437db46ed64c4dc8320b4427e124507c
Caio Romão <caioromao@gmail.com>
2012-03-27 17:19:20 -0300

    Initial implementation

---
+++ fuzzyclock.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define BUFSIZE 30
+
+char* HOUR_NAMES[] = {
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+};
+
+char* FUZZY_MSG[] = {
+    "%s o' clock\n",
+    "five past %s\n",
+    "ten past %s\n",
+    "quarter past %s\n",
+    "twenty past %s\n",
+    "twenty five past %s\n",
+    "half past %s\n",
+    "twenty five to %s\n",
+    "twenty to %s\n",
+    "quarter to %s\n",
+    "ten to %s\n",
+    "five to %s\n",
+};
+
+void get_fuzzy_time(time_t timer, char* buffer) {
+    struct tm* clock = localtime(&timer);
+
+    int index = 0;
+    int hour = clock->tm_hour;
+    int min = clock->tm_min;
+
+    if (min > 2) {
+        index = ((min - 3) / 5) - 1;
+    }
+
+    if (index > 6) { hour++; }
+
+    char* timestr = FUZZY_MSG[index];
+    char* hourname = HOUR_NAMES[(hour - 1) % 12];
+
+    snprintf(buffer, BUFSIZE, timestr, hourname);
+}
+
+
+int main(int argc, char const* argv[])
+{
+    char msg[BUFSIZE];
+    get_fuzzy_time(time(NULL), msg);
+    printf(msg);
+    return 0;
+}
